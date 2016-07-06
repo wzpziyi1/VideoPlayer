29ead6bd71f6a8187dfec5349d5d032095de650b
wangzhipan <wangzhipan@yiniu.com>
2016-07-06 10:28:56 +0800

    Initial Commit

---
+++ VideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayer
+//
+//  Created by 王志盼 on 16/7/6.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VideoPlayer
+//
+//  Created by 王志盼 on 16/7/6.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
